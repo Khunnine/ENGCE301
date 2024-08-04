3e5e7135ec9500c9375ba2f12e11a87dfb9d11b2
Natthporn Jaimon <118286306+Khunnine@users.noreply.github.com>
2024-08-04 16:31:50 +0700

    Add files via upload

---
+++ lab3/dbconfig.js
@@ -0,0 +1,19 @@
+var dbconfig = {
+    development: {
+        //connectionLimit : 10,
+        host     : 'localhost',
+        port     : '3306',
+        user     : 'root',
+        password : '',
+        database : 'moviedb1'
+    },
+    production: {
+        //connectionLimit : 10,
+        host     : 'localhost',
+        port     : '3306',
+        user     : 'root',
+        password : '',
+        database : 'moviedb1'
+    }
+    };
+module.exports = dbconfig;
